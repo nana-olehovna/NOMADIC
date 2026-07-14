7af510431d53216ea17b3f6f1ed4f8bea3e5ad10
Anastasiia <anastasiia.hainova@gmail.com>
2026-07-14 18:57:07 +0200

    Basic styles added

--- frontend/src/layouts/MainLayout/MainLayout.jsx
+++ frontend/src/layouts/MainLayout/MainLayout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom"
 import { Header } from "@/components"
+import './MainLayout.scss'
 
 export default function MainLayout() {
   return (
     <div className="mainLayoutWrapper">
       <Header />
-      <main>
+      <main className="mainLayoutContentWrapper">
         <Outlet />
       </main>
     </div>
